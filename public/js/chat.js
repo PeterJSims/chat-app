6b734cd8d79552760dca7fbed678e6aa9ced62c7
Peter Sims <ragtimesims@gmail.com>
2019-08-28 08:59:53 -0400

    event acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,5 +7,27 @@ socket.on('message', (message) => {
 document.querySelector('#send-message').addEventListener('submit', (e) => {
 	e.preventDefault();
 	const message = e.target.elements.message.value;
-	socket.emit('sendMessage', message);
+
+	socket.emit('sendMessage', message, (error) => {
+		error ? console.log(error) : console.log('Message delivered');
+	});
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if (!navigator.geolocation) {
+		return alert('Geolocation is not supported');
+	}
+
+	navigator.geolocation.getCurrentPosition((position) => {
+		socket.emit(
+			'sendLocation',
+			{
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude
+			},
+			() => {
+				console.log('Location shared!');
+			}
+		);
+	});
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,8 +13,8 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-//server(emit) -> client(receive) - countUpdated
-//client(emit) -> server(receive) - increment
+//server(emit) -> client(receive) -> acknowledgement -> countUpdated
+//client(emit) -> server(receive) -> acknowledgement -> increment
 
 io.on('connection', (socket) => {
 	console.log('new socket connection');
@@ -21,8 +22,19 @@ io.on('connection', (socket) => {
 	socket.emit('message', 'Welcome!');
 	socket.broadcast.emit('message', 'A new user has joined');
 
-	socket.on('sendMessage', (message) => {
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter();
+
+		if (filter.isProfane(message)) {
+			return callback('No profanity!');
+		}
 		io.emit('message', message);
+		callback('Delivered');
+	});
+
+	socket.on('sendLocation', (obj, callback) => {
+		io.emit('message', `https://google.com/maps?q=${obj.latitude},${obj.longitude}`);
+		callback();
 	});
 
 	socket.on('disconnect', () => {
